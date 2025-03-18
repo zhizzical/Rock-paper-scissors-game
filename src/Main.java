c08ed3d15f38211313c82e8eb61b23645aff5535
Zhina <zhinaa.adil@gmail.com>
2025-03-18 21:49:21 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //used to store the scores
+        int playerScore = 0;
+        int computerScore = 0;
 
         while (true) {
             String[] choice = {"rock", "paper", "scissors"};
@@ -10,7 +13,7 @@ public class Main {
             String computerChoice = choice[rand.nextInt(3)]; //nextint(): from 0 to n-1
 
             System.out.println("Enter your move (rock, paper, scissors) or enter exit:");
-            String playerChoice = sc.nextLine().toLowerCase();
+            String playerChoice = sc.nextLine().toLowerCase().trim();
 
             if (playerChoice.equals("exit")) {
                 System.out.println("Thank you for playing! Good bye!");
@@ -29,23 +32,29 @@ public class Main {
             //when the moves are equal:
             if (computerChoice.equals(playerChoice)) {
                 System.out.println("It's a tie!");
-
+                //add 1 point to each and print the scores
+                playerScore++;
+                computerScore++;
+                System.out.println("Your score: " + playerScore + " | Computer score: " + computerScore);
             } //win conditions:
             else if ((computerChoice.equals("rock") && playerChoice.equals("paper")) ||
                     (computerChoice.equals("paper") && playerChoice.equals("scissors")) ||
                     (computerChoice.equals("scissors") && playerChoice.equals("rock"))) {
-
+                playerScore++;
                 System.out.println("You win! The computer bows to your greatness.");
+                System.out.println("Your score: " + playerScore + " | Computer score: " + computerScore);
             }
             else {
+                computerScore++;
                 System.out.println("You lose! The computer outsmarted you this time.");
+                System.out.println("Your score: " + playerScore  + " | Computer score: " + computerScore);
             }
 
             //asks the player if they want to play another round
             String playAgain = "yes";
             while(true) {
                 System.out.println("Do you want to play again? (yes/no): ");
-                playAgain = sc.nextLine().toLowerCase();
+                playAgain = sc.nextLine().toLowerCase().trim();
                 //break out of this loop but not the other
                 if (playAgain.equals("yes")) {
                     break;
